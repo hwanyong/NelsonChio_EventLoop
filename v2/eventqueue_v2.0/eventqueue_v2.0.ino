3840f2c2b75a24fa6b68c0621ab3776755cce274
UHD <yoo.hwanyong@gmail.com>
2020-06-04 19:02:41 -0700

    setting

--- v2/eventqueue_v2.0/eventqueue_v2.0.ino
+++ v2/eventqueue_v2.0/eventqueue_v2.0.ino
@@ -1,10 +1,15 @@
 #include <Wire.h>  
 #include <Adafruit_SSD1306.h>
+#include <bitset>
+
+using byte = unsigned char;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(0x3c, SDA, SCL);
+void IRAM_ATTR onTimer();
 
 void setup() {
+    Serial.begin(115200);
 }
 
 void loop() {
