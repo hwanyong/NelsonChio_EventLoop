fa86e6a5c5e4d2b11d250d32533e1df2a20935a3
UHD <yoo.hwanyong@gmail.com>
2020-06-30 23:41:06 -0700

    calibration source

--- platformio/CalibrationScale/src/main.cpp
+++ platformio/CalibrationScale/src/main.cpp
@@ -1,20 +1,51 @@
 #include <Arduino.h>
+#include "HX711.h"
 
-#define PINBUTTON 21
+#define PIN_BUTTON			21
+#define PIN_LOADCELL_DOUT	13
+#define PIN_LOADCELL_SCK	12
+
+HX711 scale;
+long calivalue = 0;
+
 int btnState = 0;
+int bfBtnState = 0; // { HIGH, LOW}
 
 void setup() {
 	Serial.begin(115200);
-	pinMode(PINBUTTON, INPUT);
+	pinMode(PIN_BUTTON, INPUT);
+
+	scale.begin(PIN_LOADCELL_DOUT, PIN_LOADCELL_SCK);
+	scale.power_down();
 }
 
 void loop() {
-	btnState = digitalRead(PINBUTTON);
+	btnState = digitalRead(PIN_BUTTON);
+
+	char cmd;
+	if (Serial.available()) {
+		cmd = (char)Serial.read();
+	}
 
 	if (btnState == HIGH) {
-		Serial.println("HIGH");
+		if (bfBtnState != btnState) {
+			scale.power_up();
+		}
 	}
 	else {
-		Serial.println("LOW");
+		if (bfBtnState != btnState) {
+			scale.power_down();
+		}
+
+		switch (cmd)
+		{
+		case 'i': // initialize
+			break;
+		case 'r': // reset (set zero)
+			scale.tare();
+			break;
+		default:
+			break;
+		}
 	}
 }
