887a6b331410b4767e64b5482873a7f792247c08
UHD <yoo.hwanyong@gmail.com>
2020-07-01 18:10:47 -0700

    set calibration factor

--- platformio/CalibrationMotor/src/main.cpp
+++ platformio/CalibrationMotor/src/main.cpp
@@ -4,47 +4,57 @@
 #define PIN_BUTTON			21
 #define PIN_LOADCELL_DOUT	13
 #define PIN_LOADCELL_SCK	12
+#define SCALE_CLBUNIT		0.035274
 
 HX711 scale;
-long calivalue = 0;
 
-int btnState = 0;
-int bfBtnState = 0; // { HIGH, LOW}
+float calibration_factor = 1867.57;
+
+int btnState = -1;
+int bfBtnState = -1; // { HIGH, LOW}
 
 void setup() {
 	Serial.begin(115200);
 	pinMode(PIN_BUTTON, INPUT);
 
 	scale.begin(PIN_LOADCELL_DOUT, PIN_LOADCELL_SCK);
-	scale.power_down();
+	scale.set_scale(calibration_factor);
+	scale.tare();
 }
 
 void loop() {
 	btnState = digitalRead(PIN_BUTTON);
 
-	char cmd;
-	if (Serial.available()) {
-		cmd = (char)Serial.read();
-	}
+	char cmd = ' ';
+	if (Serial.available()) { cmd = (char)Serial.read(); }
 
+	// Serial.println(String(btnState) + ":" + String(bfBtnState));
 	if (btnState == HIGH) {
 		if (bfBtnState != btnState) {
-			scale.power_up();
+			Serial.println("BUTTON: DOWN");
+			bfBtnState = btnState;
+			// scale.power_up();
 		}
+
+		String log = "";
+		float units = scale.get_units();
+
+		if (units < 0) units = 0.0;
+		Serial.println((units * SCALE_CLBUNIT), 4);
 	}
 	else {
 		if (bfBtnState != btnState) {
-			scale.power_down();
+			Serial.println("BUTTON: UP");
+			bfBtnState = btnState;
+			// scale.power_down();
 		}
 
 		switch (cmd)
 		{
-		case 'i': // initialize
-			break;
-		case 'r': // reset (set zero)
+		case 'z': // reset (set zero factor)
 			scale.tare();
 			break;
-		default:
+		break;
 			break;
 		}
 	}
--- platformio/CalibrationScale/src/main.cpp
+++ platformio/CalibrationScale/src/main.cpp
@@ -7,57 +7,61 @@
  Arduino pin 5V -> HX711 VCC
  Arduino pin GND -> HX711 GND 
 */
-
+#include <Arduino.h>
 #include "HX711.h"
 
 #define PIN_LOADCELL_DOUT	13
 #define PIN_LOADCELL_SCK	12
 HX711 scale;
 
-float calibration_factor = -14; // this calibration factor is adjusted according to my load cell
+float calibration_factor = 1867.57; // this calibration factor is adjusted according to my load cell
 float units;
 float ounces;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("HX711 calibration sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
+	Serial.begin(115200);
 	scale.begin(PIN_LOADCELL_DOUT, PIN_LOADCELL_SCK);
 
-  scale.set_scale();
-  scale.tare();  //Reset the scale to 0
+	scale.set_scale(calibration_factor);
+	scale.tare(); //Reset the scale to 0
 
-  long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
+	// long zero_factor = scale.read_average(); //Get a baseline reading
+	// Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+	// Serial.println(zero_factor);
 }
 
 void loop() {
+	scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
-  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+	Serial.print("Reading: ");
+	units = scale.get_units();
+	// units = scale.get_units(5);
+	// units = scale.read_average(4);
+	// units = scale.get_value(4);
+	// if (units < 0) units = 0.00;
 
-  Serial.print("Reading: ");
-  units = scale.get_units();
-  if (units < 0)
-  {
-    units = 0.00;
-  }
-  ounces = units * 0.035274;
-  Serial.print(units);
-  Serial.print(" grams"); 
-  Serial.print(" calibration_factor: ");
-  Serial.print(calibration_factor);
-  Serial.println();
+	ounces = units * 0.035274;
+	Serial.print(units, 4);
+	Serial.print(" grams"); 
+	Serial.print(" calibration_factor: ");
+	Serial.print(calibration_factor, 6);
+	Serial.println();
 
-  if(Serial.available())
-  {
-    char temp = Serial.read();
-    if(temp == '+' || temp == 'a')
-      calibration_factor += 1;
-    else if(temp == '-' || temp == 'z')
-      calibration_factor -= 1;
-  }
+	if(Serial.available())
+	{
+		char temp = Serial.read();
+
+		switch (temp)
+		{
+		case 'a': calibration_factor += 0.01; break;
+		case 's': calibration_factor += 0.1; break;
+		case 'd': calibration_factor += 1; break;
+		case 'f': calibration_factor += 10; break;
+
+		case 'z': calibration_factor -= 0.01; break;
+		case 'x': calibration_factor -= 0.1; break;
+		case 'c': calibration_factor -= 1; break;
+		case 'v': calibration_factor -= 10; break;
+		}
+	}
 }
